d115aee9f0f351514e7456f30229de55e8a2965f
Faithman <79011273+Faithmanex@users.noreply.github.com>
2023-04-21 10:00:37 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,7 @@
-/* Template: Evolo - StartUp HTML Landing Page Template
-   Author: Inovatik
-   Created: June 2019
-   Description: Custom JS file
-*/
-
-
 (function($) {
     "use strict"; 
 	
-	/* Preloader */
+	
 	$(window).on('load', function() {
 		var preloaderFadeOutTime = 500;
 		function hidePreloader() {
