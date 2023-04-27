5ec828b16ba3de1519725731cacec2e20ff65733
Faithman <79011273+Faithmanex@users.noreply.github.com>
2023-04-27 04:37:08 -0700

    Update

--- php/requestform-process.php
+++ php/requestform-process.php
@@ -31,7 +31,7 @@ if (empty($_POST["terms"])) {
     $terms = $_POST["terms"];
 }
 
-$EmailTo = "yourname@domain.com";
+$EmailTo = "fmanekponnee@gmail.com";
 $Subject = "New meeting request from Evolo landing page";
 
 // prepare email body text
